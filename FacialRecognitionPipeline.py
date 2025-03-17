3c25d886510ecc5d5dae941ee563fa348bb4299b
Jack Good <jacklianggood@gmail.com>
2025-03-17 14:42:51 -0400

    Update FacialRecognitionPipeline.py

--- FacialRecognitionPipeline.py
+++ FacialRecognitionPipeline.py
@@ -1,1 +1,22 @@
+import os
+import cv2
+import json
+from PIL import Image
 from deepface import DeepFace
+
+# Detect face
+img1_path = "data/derek1.jpg"
+img2_path = "data/derek3.jpg"
+
+result = DeepFace.verify(
+  img1_path = img1_path,
+  img2_path = img2_path,
+)
+print(json.dumps(result, indent = 2))
+
+# Find face
+dfs = DeepFace.find(
+  img_path = "data/derek1.jpg",
+  db_path = "data",
+)
+print(dfs)
